59c05155d4e1504bcfeec0fdfba6f3be1e83c420
Aymon de Broglie <aymon.debroglie@akur8.com>
2022-03-25 19:11:57 +0100

    add hashing , have to do the logic now

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -21,8 +21,10 @@ export class GitService {
     }
 
     async cloneRepo(repoUrl: string) {
-        // await this.pfs.mkdir(this.dir);
-        console.log(repoUrl);
+        const dirName  = this.hash(repoUrl);
+        console.log(dirName)
+
+        this.pfs.readdir(dirName).then().catch()
         await git.clone({
             fs : this.fs,
             http,
@@ -35,6 +37,19 @@ export class GitService {
         }).then(r => console.log( 'done'));
         console.log("hello");
         await this.pfs.readdir(this.dir).then(console.log);
+    }
+
+    hash(s : string , seed = 186) : string {
+        // taken from stackoverflow  https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+        let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+        for (let i = 0, ch; i < s.length; i++) {
+            ch = s.charCodeAt(i);
+            h1 = Math.imul(h1 ^ ch, 2654435761);
+            h2 = Math.imul(h2 ^ ch, 1597334677);
+        }
+        h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+        h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+        return String(4294967296 * (2097151 & h2) + (h1 >>> 0));
     }
 
 }
